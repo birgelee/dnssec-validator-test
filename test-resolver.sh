cee0335fc3f3d7246aefa33fd8f03fa5a6b1d0b9
Henry Birge-Lee <birgelee@princeton.edu>
2025-05-02 16:17:19 -0400

    ability to speicfy system resolver

--- test-resolver.sh
+++ test-resolver.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-DNSSEC_RESOLVER=$1
+if [ -z "$1" ]; then
+    echo "------Using default system resolver"
+    DNSSEC_RESOLVER=""
+else
+    echo "------Using $1 resolver"
+    DNSSEC_RESOLVER="@$1"
+fi
 
 echo "------Testing proper implementation of the DNSSEC validation algorithm RFC 4035 Section 5"
-EXPIRED_RESULT="$(dig @$DNSSEC_RESOLVER expired.caatestsuite-dnssec.com IN CAA \
+EXPIRED_RESULT="$(dig $DNSSEC_RESOLVER expired.caatestsuite-dnssec.com IN CAA \
 )"
 if echo $EXPIRED_RESULT | grep -q "status: SERVFAIL" ; then
     echo "expired.caatestsuite-dnssec.com PASSED"
@@ -13,7 +19,7 @@ else
 fi
 
 
-MISSING_RESULT="$(dig @$DNSSEC_RESOLVER missing.caatestsuite-dnssec.com IN CAA \
+MISSING_RESULT="$(dig $DNSSEC_RESOLVER missing.caatestsuite-dnssec.com IN CAA \
 )"
 if echo $MISSING_RESULT | grep -q "status: SERVFAIL" ; then
     echo "missing.caatestsuite-dnssec.com PASSED"
@@ -22,7 +28,7 @@ else
     exit 1
 fi
 
-BLACKHOLE_RESULT="$(dig @$DNSSEC_RESOLVER blackhole.caatestsuite-dnssec.com IN CAA \
+BLACKHOLE_RESULT="$(dig $DNSSEC_RESOLVER blackhole.caatestsuite-dnssec.com IN CAA \
 )"
 if echo $BLACKHOLE_RESULT | grep -q "status: SERVFAIL" ; then
     echo "blackhole.caatestsuite-dnssec.com PASSED"
@@ -31,7 +37,7 @@ else
     exit 1
 fi
 
-SERVFAIL_RESULT="$(dig @$DNSSEC_RESOLVER servfail.caatestsuite-dnssec.com IN CAA \
+SERVFAIL_RESULT="$(dig $DNSSEC_RESOLVER servfail.caatestsuite-dnssec.com IN CAA \
 )"
 if echo $SERVFAIL_RESULT | grep -q "status: SERVFAIL" ; then
     echo "servfail.caatestsuite-dnssec.com PASSED"
@@ -41,7 +47,7 @@ else
 fi
 
 
-REFUSED_RESULT="$(dig @$DNSSEC_RESOLVER refused.caatestsuite-dnssec.com IN CAA \
+REFUSED_RESULT="$(dig $DNSSEC_RESOLVER refused.caatestsuite-dnssec.com IN CAA \
 )"
 if echo $REFUSED_RESULT | grep -q "status: SERVFAIL" ; then
     echo "refused.caatestsuite-dnssec.com PASSED"
@@ -51,7 +57,7 @@ else
 fi
 
 echo "------Test support for digest type 2 (SHA-2) RFC 4509, RRSIG type 8 (SHA-2 over RSA) RFC 5702, and optional RRSIG type 13."
-CLOUDFLARE_CAA_RESULT="$(dig @$DNSSEC_RESOLVER cloudflare.com IN CAA \
+CLOUDFLARE_CAA_RESULT="$(dig $DNSSEC_RESOLVER cloudflare.com IN CAA \
 )"
 if echo $CLOUDFLARE_CAA_RESULT | grep -q "status: NOERROR" && echo $CLOUDFLARE_CAA_RESULT | grep -q "flags:[a-z ]*ad" ; then
     echo "cloudflare.com test PASSED"
@@ -61,7 +67,7 @@ else
 fi
 
 echo "------Test support for NSEC RFC 4035 Section 5."
-CLOUDFLARE_NXSUBDOMAIN_RESULT="$(dig @$DNSSEC_RESOLVER a-subdomain-that-does-not-exist.cloudflare.com IN A \
+CLOUDFLARE_NXSUBDOMAIN_RESULT="$(dig $DNSSEC_RESOLVER a-subdomain-that-does-not-exist.cloudflare.com IN A \
 )"
 if echo $CLOUDFLARE_NXSUBDOMAIN_RESULT | grep -q "status: NOERROR" && echo $CLOUDFLARE_NXSUBDOMAIN_RESULT | grep -q "ANSWER: 0" && echo $CLOUDFLARE_NXSUBDOMAIN_RESULT | grep -q "flags:[a-z ]*ad" ; then
     echo "a-subdomain-that-does-not-exist.cloudflare.com test PASSED"
@@ -71,7 +77,7 @@ else
 fi
 
 echo "------Test support for NSEC3 RFC 5155."
-CN_RESULT="$(dig @$DNSSEC_RESOLVER cn IN A \
+CN_RESULT="$(dig $DNSSEC_RESOLVER cn IN A \
 )"
 if echo $CN_RESULT | grep -q "status: NOERROR" && echo $CN_RESULT | grep -q "ANSWER: 0" && echo $CN_RESULT | grep -q "flags:[a-z ]*ad" ; then
     echo "cn test PASSED"
